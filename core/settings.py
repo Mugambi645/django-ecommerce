7d81680ba346dd5f13a9c2da729b9b803c8fa8c9
patrick mugambi <patrick@kali.patrick>
2025-04-27 14:16:19 +0300

    create model userbase and apply migrations

--- core/settings.py
+++ core/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "store",
     "basket",
-    
+    "account",
 ]
 
 MIDDLEWARE = [
@@ -133,3 +133,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+#Custom user Model
+AUTH_USER_MODEL = "account.UserBase"
+LOGIN_REDIRECT_URL = "/account/dashboard"
+LOGIN_URL = "/account/login/"
